84c39c4c673776c85e34bd35151365cf0d7abbe0
Blazeflack <blazeflack@hotmail.com>
2012-09-16 02:06:33 +0200

    english.lua: add 1 locale

--- english.lua
+++ english.lua
@@ -781,6 +781,7 @@ L["Per Row"] = true;
 L["Percent"] = true;
 L["Pet Frame"] = true;
 L["PetTarget Frame"] = true;
+L['Player Frame Aura Bars'] = true;
 L["Player Frame"] = true;
 L["Portrait"] = true;
 L["Position"] = true;
