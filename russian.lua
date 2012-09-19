d88c16ff50c9359dce356eb02867654c43da3441
Blazeflack <blazeflack@hotmail.com>
2012-09-19 12:47:35 +0200

    portuguese.lua: manually merge suggestions by heltonaugusto on github

--- russian.lua
+++ russian.lua
@@ -1,4 +1,4 @@
-﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI", "ruRU")
 if not L then return; end
 
--- taiwan.lua
+++ taiwan.lua
@@ -1,4 +1,4 @@
-﻿-- 繁體中文版由 elmush 翻譯
+-- 繁體中文版由 elmush 翻譯
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI", "zhTW")
 if not L then return end
